2c409c06efe1b33c94ee4486700bcdbb54357017
Dominik Roszkowski <dominik@roszkowski.dev>
2019-10-20 21:29:41 +0200

    Use bloc delegate to log transitions

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,7 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:conferenceapp/utils/bloc_delegate.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 
 void mainCommon() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+
   runApp(MyApp(title: "Flutter Europe"));
 }
