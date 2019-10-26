1cc51e65185619c0fcee1b6f9600f0bd6aa4f088
Dominik Roszkowski <dominik@roszkowski.dev>
2019-10-27 01:15:52 +0200

    Improve a bit authenticator debug button

--- lib/profile/profile_page.dart
+++ lib/profile/profile_page.dart
@@ -1,10 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:conferenceapp/agenda/repository/mock_talks_repository.dart';
-import 'package:conferenceapp/model/talk_list.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'authenticator_button.dart';
 import 'settings_toggle.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -34,50 +31,7 @@ class _ProfilePageState extends State<ProfilePage> {
               onChanged: (_) {},
               value: false,
             ),
-            FlatButton(
-              child: Text('Authenticate'),
-              onPressed: () async {
-                await showDialog(
-                    context: context,
-                    barrierDismissible: true,
-                    builder: (context) {
-                      return SimpleDialog(
-                        title: Text('Login to continue'),
-                        children: <Widget>[
-                          FlatButton(
-                            child: Text('Login'),
-                            onPressed: () async {
-                              final FirebaseAuth _auth = FirebaseAuth.instance;
-                              try {
-                                final result = await _auth.signInWithEmailAndPassword(
-                                  //TODO place credentials here
-                                  email: '',
-                                  password: '',
-                                );
-                                print(result);
-                              } catch (e) {
-                                print(e);
-                              }
-                            },
-                          )
-                        ],
-                      );
-                    });
-              },
-            ),
-            FlatButton(
-              child: Text('Update mock data in Firestore'),
-              onPressed: () {
-                try {
-                  // final data =
-                  //     // MockTalksRepository().talks.where((d) => d.dateTime.isAfter(DateTime(2020, 1, 24))).toList();
-                  // final list = TalkList(DateTime(2020, 1, 23), data);
-                  // Firestore.instance.collection('talks').document().setData(list.toJson());
-                } catch (e) {
-                  print(e);
-                }
-              },
-            )
+            AuthenticatorButton(),
           ],
         ),
       ),
