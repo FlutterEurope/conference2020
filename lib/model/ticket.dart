ed58864d1315571e95b9bb08eb0ebd790356d741
Dominik Roszkowski <dominik@roszkowski.dev>
2019-11-12 18:05:52 +0100

    Add basic generated ticket model

---
+++ lib/model/ticket.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:conferenceapp/utils/firestore_utils.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'ticket.g.dart';
+
+@JsonSerializable(
+  nullable: false,
+  explicitToJson: true,
+  anyMap: true,
+)
+class Ticket {
+  Ticket(this.orderId, this.ticketId, this.email, this.type);
+
+  final String orderId;
+  final String ticketId;
+  final String email;
+  final TicketType type;
+
+  factory Ticket.fromJson(Map<String, dynamic> json) => _$TicketFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TicketToJson(this);
+}
+
+enum TicketType{
+  Blind,
+  EarlyBird,
+  Regular,
+  LateForTheParty,
+  Student
+}
