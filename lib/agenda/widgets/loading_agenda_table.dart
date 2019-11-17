876545444fcca42486a9aae29e096a67cac3c8f4
Dominik Roszkowski <dominik@roszkowski.dev>
2019-11-17 20:47:51 +0100

    Extract loading view of agenda

---
+++ lib/agenda/widgets/loading_agenda_table.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoadingAgendaTable extends StatelessWidget {
+  const LoadingAgendaTable({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
