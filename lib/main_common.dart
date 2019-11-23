9d942a5b16ff0f51b34da1e3017ebb5b1c45fa11
Dominik Roszkowski <dominik@roszkowski.dev>
2019-11-23 17:17:55 +0100

    Provide shared preferences from startup

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -5,6 +5,7 @@ import 'package:conferenceapp/utils/bloc_delegate.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'analytics.dart';
 import 'app.dart';
@@ -15,8 +16,15 @@ void mainCommon() {
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runZoned<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
     analytics = FirebaseAnalytics();
+    final sharedPrefs = await SharedPreferences.getInstance();
 
-    runApp(MyApp(title: "Flutter Europe"));
+    runApp(
+      MyApp(
+        title: "Flutter Europe",
+        sharedPreferences: sharedPrefs,
+      ),
+    );
   }, onError: Crashlytics.instance.recordError);
 }
