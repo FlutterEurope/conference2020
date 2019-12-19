a46624fa24a3fa2f5c57a82030ffe11ce8dd483a
Dominik Roszkowski <dominik@roszkowski.dev>
2019-12-19 16:11:56 +0100

    Fix layout of agenda page

--- lib/agenda/agenda_page.dart
+++ lib/agenda/agenda_page.dart
@@ -2,6 +2,7 @@ import 'package:conferenceapp/agenda/bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'day_selector.dart';
 import 'widgets/loading_agenda_table.dart';
 import 'widgets/populated_agenda_table.dart';
 
@@ -34,22 +35,29 @@ class _AgendaPageState extends State<AgendaPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder(
-      bloc: BlocProvider.of<AgendaBloc>(context),
-      builder: (context, AgendaState state) => Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          state is PopulatedAgendaState
-              ? PopulatedAgendaTable(
-                  state.talks,
-                  state.rooms,
-                  pageController,
-                  currentIndex,
-                )
-              : LoadingAgendaTable(),
-        ],
-      ),
+    return Column(
+      children: <Widget>[
+        DaySelectorContainer(pageController, currentIndex.value),
+        Flexible(
+          child: BlocBuilder(
+            bloc: BlocProvider.of<AgendaBloc>(context),
+            builder: (context, AgendaState state) =>
+                state is PopulatedAgendaState
+                    ? PopulatedAgendaTable(
+                        state.talks,
+                        state.rooms,
+                        pageController,
+                      )
+                    : LoadingAgendaTable(),
+          ),
+        ),
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
   }
 }
