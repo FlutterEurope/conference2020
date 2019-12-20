bcdda22e3295b7fa7939a10fe67a14a0b518393a
Dominik Roszkowski <dominik@roszkowski.dev>
2019-12-20 15:38:11 +0100

    Extend save button

--- lib/ticket/widgets/save_ticket_button.dart
+++ lib/ticket/widgets/save_ticket_button.dart
@@ -4,24 +4,34 @@ import 'package:line_icons/line_icons.dart';
 class SaveTicketButton extends StatelessWidget {
   const SaveTicketButton({
     Key key,
+    this.enabled,
+    this.onSave,
   }) : super(key: key);
+
+  final bool enabled;
+  final VoidCallback onSave;
 
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
       color: Colors.green,
-      shape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Text('Save'),
+          Text(
+            'Save',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
           SizedBox(width: 6),
-          Icon(LineIcons.check_circle)
+          Icon(
+            LineIcons.check_circle,
+            color: Colors.white,
+          )
         ],
       ),
-      onPressed: () {},
+      onPressed: enabled ? onSave : null,
     );
   }
 }
