18c2af1d5a0a1d55c4c9a8ed484e5e60d5f367f0
Dominik Roszkowski <dominik@roszkowski.dev>
2020-01-01 22:45:42 +0100

    Add flavor name to config

--- lib/config.dart
+++ lib/config.dart
@@ -3,8 +3,13 @@ import 'package:flutter/foundation.dart';
 class AppConfig {
   final String contentfulSpace;
   final String contentfulApiKey;
+  final String flavor;
 
-  AppConfig({@required this.contentfulSpace, @required this.contentfulApiKey});
+  AppConfig({
+    @required this.contentfulSpace,
+    @required this.contentfulApiKey,
+    @required this.flavor,
+  });
 }
 
 AppConfig appConfig;
