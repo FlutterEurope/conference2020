194c9030a230fc1508250d49700ca49001f82b40
Dominik Roszkowski <dominik@roszkowski.dev>
2020-01-01 18:06:34 +0100

    Fix appearance of settings toggle

--- lib/profile/settings_toggle.dart
+++ lib/profile/settings_toggle.dart
@@ -14,17 +14,11 @@ class SettingsToggle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Flexible(child: Text(title)),
-          Switch(
-            onChanged: onChanged,
-            value: value,
-          ),
-        ],
+    return ListTile(
+      title: Text(title),
+      trailing: Switch(
+        onChanged: onChanged,
+        value: value,
       ),
     );
   }
