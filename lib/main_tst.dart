1850b0ca860d03d39a68ee5a6d85b3bfd22dd62b
Dominik Roszkowski <dominik@roszkowski.dev>
2020-01-07 21:16:47 +0100

    Add config placeholders

--- lib/main_tst.dart
+++ lib/main_tst.dart
@@ -4,10 +4,14 @@ import 'package:conferenceapp/main_common.dart';
 void main() {
   final space = 'CONTENTFUL_SPACE';
   final apiKey = 'CONTENTFUL_API_KEY';
+  final snapfeedProjectId = 'SNAPFEED_PROJECTID';
+  final snapfeedSecret = 'SNAPFEED_SECRET';
   final config = AppConfig(
     contentfulApiKey: apiKey,
     contentfulSpace: space,
     flavor: 'tst',
+    snapfeedProjectId: snapfeedProjectId,
+    snapfeedSecret: snapfeedSecret,
   );
   mainCommon(config: config);
 }
