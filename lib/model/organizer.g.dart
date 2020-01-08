2bf39cbb2f635728bc8007ba3fa5cea16b19ec92
Dominik Roszkowski <dominik@roszkowski.dev>
2020-01-08 11:32:58 +0100

    Add organizer model

---
+++ lib/model/organizer.g.dart
@@ -0,0 +1,57 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'organizer.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Organizers _$OrganizersFromJson(Map json) {
+  return Organizers(
+    total: json['total'] as int,
+    skip: json['skip'] as int,
+    limit: json['limit'] as int,
+    items: (json['items'] as List)
+        ?.map((e) => e == null ? null : OrganizerFields.fromJson(e as Map))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$OrganizersToJson(Organizers instance) =>
+    <String, dynamic>{
+      'total': instance.total,
+      'skip': instance.skip,
+      'limit': instance.limit,
+      'items': instance.items,
+    };
+
+OrganizerFields _$OrganizerFieldsFromJson(Map json) {
+  return OrganizerFields(
+    SystemFields.fromJson(Map<String, dynamic>.from(json['sys'] as Map)),
+    Organizer.fromJson(json['fields'] as Map),
+  );
+}
+
+Map<String, dynamic> _$OrganizerFieldsToJson(OrganizerFields instance) =>
+    <String, dynamic>{
+      'sys': instance.sys.toJson(),
+      'fields': instance.fields.toJson(),
+    };
+
+Organizer _$OrganizerFromJson(Map json) {
+  return Organizer(
+    json['name'] as String,
+    Asset.fromJson(Map<String, dynamic>.from(json['picture'] as Map)),
+    _documentFromJson(json['bio'] as Map),
+    _documentFromJson(json['longBio'] as Map),
+    json['order'] as int,
+  );
+}
+
+Map<String, dynamic> _$OrganizerToJson(Organizer instance) => <String, dynamic>{
+      'name': instance.name,
+      'picture': instance.picture.toJson(),
+      'bio': _documentToJson(instance.bio),
+      'longBio': _documentToJson(instance.longBio),
+      'order': instance.order,
+    };
