5466e18c39a028a975d350761842542bdf5ba694
Dominik Roszkowski <dominik@roszkowski.dev>
2020-01-09 15:14:02 +0100

    Log method names and classes to Bugfender

--- lib/common/logger.dart
+++ lib/common/logger.dart
@@ -1,27 +1,41 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bugfender/flutter_bugfender.dart';
+import 'package:stack_trace/stack_trace.dart';
 
 class Logger {
   static bool printToConsole = false;
 
   static info(String message) {
+    final className = Trace.current().frames[1].member.split(".")[0];
+    final methodName = Trace.current().frames[1].member.split(".")[1];
     if (printToConsole) debugPrint(message);
-    FlutterBugfender.log(message);
+    FlutterBugfender.l(message,
+        tag: 'Info', methodName: methodName, className: className);
   }
 
   static error(String message) {
+    final className = Trace.current().frames[1].member.split(".")[0];
+    final methodName = Trace.current().frames[1].member.split(".")[1];
     if (printToConsole) debugPrint(message);
-    FlutterBugfender.error(message);
+    FlutterBugfender.l(message,
+        tag: 'Error', methodName: methodName, className: className);
+    FlutterBugfender.forceSendOnce();
   }
 
   static errorException(Exception e, [StackTrace s]) {
     if (printToConsole) print(e);
     if (printToConsole && s != null) print(s);
+
     FlutterBugfender.error('Exception: $e.\nStacktrace: $s');
+    FlutterBugfender.forceSendOnce();
   }
 
   static warn(String message) {
+    final className = Trace.current().frames[1].member.split(".")[0];
+    final methodName = Trace.current().frames[1].member.split(".")[1];
     if (printToConsole) debugPrint(message);
-    FlutterBugfender.warn(message);
+    FlutterBugfender.l(message,
+        tag: 'Warn', methodName: methodName, className: className);
+    FlutterBugfender.forceSendOnce();
   }
 }
