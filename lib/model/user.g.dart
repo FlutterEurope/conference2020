b42ea002a026b391f94649f4a4bbf6caa56ddf86
Dominik Roszkowski <dominik@roszkowski.dev>
2020-01-11 00:07:03 +0100

    Add ticketId field to user model

--- lib/model/user.g.dart
+++ lib/model/user.g.dart
@@ -10,10 +10,12 @@ User _$UserFromJson(Map json) {
   return User(
     json['userId'] as String,
     (json['favoriteTalksIds'] as List).map((e) => e as String).toList(),
+    json['ticketId'] as String,
   );
 }
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'favoriteTalksIds': instance.favoriteTalksIds,
       'userId': instance.userId,
+      'ticketId': instance.ticketId,
     };
