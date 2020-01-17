93413d9a410f297310ece7c7db0f866f831d8cfa
Korzonkiee <mck.korzeniewski@gmail.com>
2020-01-17 23:57:49 +0100

    Fetch config parameter when showing error message

--- lib/rate/effects/show_rating_talk_too_early_error.dart
+++ lib/rate/effects/show_rating_talk_too_early_error.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void showRatingTalkTooEarlyError(BuildContext context) {
+  final config = Provider.of<RemoteConfig>(context, listen: false);
+  final minutes = config?.getInt("minutes_before_talk_can_be_rated") ?? 5;
+
   Scaffold.of(context).removeCurrentSnackBar();
   Scaffold.of(context).showSnackBar(
     SnackBar(
       content: Text(
-          "Talk can be rated 5 minutes before the presentation is finished."),
+          "Talk can be rated $minutes minutes before the presentation is finished."),
       behavior: SnackBarBehavior.floating,
       backgroundColor: Theme.of(context).accentColor,
     ),
