9564acf5ca04067695f969f4ba8b9ad1b28b13e9
Dominik Roszkowski <dominik@roszkowski.dev>
2020-01-21 13:30:19 +0100

    Crude check before party

--- lib/ticket_check/scan_party_page.dart
+++ lib/ticket_check/scan_party_page.dart
@@ -4,15 +4,33 @@ import 'package:fast_qr_reader_view/fast_qr_reader_view.dart';
 import 'package:flutter/material.dart';
 import 'package:vibration/vibration.dart';
 
-class ScanPartyPage extends StatefulWidget {
+class ScanPartyPage extends StatelessWidget {
   final List<QrCameraDescription> cameras;
 
   const ScanPartyPage({Key key, this.cameras}) : super(key: key);
+
   @override
-  _ScanPartyPageState createState() => _ScanPartyPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ticket scanning at the party'),
+      ),
+      body: ScanPartyPageContent(
+        cameras: cameras,
+      ),
+    );
+  }
 }
 
-class _ScanPartyPageState extends State<ScanPartyPage> {
+class ScanPartyPageContent extends StatefulWidget {
+  final List<QrCameraDescription> cameras;
+
+  const ScanPartyPageContent({Key key, this.cameras}) : super(key: key);
+  @override
+  _ScanPartyPageContentState createState() => _ScanPartyPageContentState();
+}
+
+class _ScanPartyPageContentState extends State<ScanPartyPageContent> {
   QRReaderController controller;
   bool scanning = true;
   final collection = Firestore.instance.collection('party');
@@ -25,25 +43,26 @@ class _ScanPartyPageState extends State<ScanPartyPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ticket scanning at the party'),
-      ),
-      body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            StreamBuilder<QuerySnapshot>(
-              stream: collection.snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final List array = snapshot.data.documents;
-                  final length = array.length;
-                  return Positioned(
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      color: Colors.black45,
+    return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          Center(
+            child: _cameraPreviewWidget(),
+          ),
+          StreamBuilder<QuerySnapshot>(
+            stream: collection.snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final List array = snapshot.data.documents;
+                final length = array.length;
+                return Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    color: Colors.black45,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
                       child: Text(
                         '$length',
                         style: TextStyle(
@@ -53,29 +72,26 @@ class _ScanPartyPageState extends State<ScanPartyPage> {
                         textAlign: TextAlign.center,
                       ),
                     ),
-                  );
-                }
-                return Container();
-              },
-            ),
-            Center(
-              child: _cameraPreviewWidget(),
-            ),
-            if (!scanning)
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    onPressed: startScanning,
-                    child: Text('Scan again'),
                   ),
+                );
+              }
+              return Container();
+            },
+          ),
+          if (!scanning)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: startScanning,
+                  child: Text('Scan again'),
                 ),
-              )
-          ],
-        ),
+              ),
+            )
+        ],
       ),
     );
   }
@@ -133,38 +149,52 @@ class _ScanPartyPageState extends State<ScanPartyPage> {
     }
   }
 
-  void onCodeRead(dynamic value) {
-    logger.info("Stopping scanning, value detected: $value");
+  void onCodeRead(dynamic value) async {
+    logger.info("Party ticket scanned, value detected: $value");
     Vibration.vibrate(duration: 300);
 
-    // setState(() {
-    //   scanning = false;
-    // });
     try {
       final values = value.toString().split(' ');
       final id = values[0];
-      collection.document('$id').setData(
-        {
-          'updated': Timestamp.now(),
-          'orderId': values[1],
-          'ticketId': values[2],
-        },
-      );
-      // controller.stopScanning();
+      final orderId =
+          values[1].contains('OT') ? values[1].substring(2) : values[1];
+      final ticketId = values[2];
+
+      final doc = await collection.document('$id').get();
+      if (doc?.exists == true) {
+        showInSnackBar('Uczestnik już skanował bilet na imprezę');
+      }
+
+      final matchingTickets = await getMatchingTickets(orderId, ticketId);
+
+      if (matchingTickets.length > 0) {
+        final newDoc = await collection.document('$id').setData(
+          {
+            'updated': Timestamp.now(),
+            'orderId': values[1],
+            'ticketId': values[2],
+          },
+        );
+        showInSnackBar('Ok, bilet prawidłowy ✓');
+      } else {
+        showInSnackBar('Nie ma takiego biletu ❗️❗️❗️❗️', Colors.red);
+      }
     } catch (e, s) {
       logger.errorException(e, s);
     }
+    startScanning();
   }
 
-  void startScanning() {
+  void startScanning() async {
     logger.info("Starting scanning");
     try {
-      if (scanning == false) controller.stopScanning();
+      await controller.stopScanning();
     } catch (e) {
       logger.errorException(e);
     }
+    await Future.delayed(Duration(milliseconds: 1000));
     try {
-      controller.startScanning();
+      await controller.startScanning();
       setState(() {
         scanning = true;
       });
@@ -173,10 +203,36 @@ class _ScanPartyPageState extends State<ScanPartyPage> {
     }
   }
 
-  void showInSnackBar(String message) {
-    Scaffold.of(context).showSnackBar(new SnackBar(
-      content: new Text(message),
+  void showInSnackBar(String message, [Color color = Colors.green]) {
+    Scaffold.of(context).hideCurrentSnackBar();
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text(message),
       behavior: SnackBarBehavior.floating,
+      backgroundColor: color,
     ));
+  }
+
+  Future<List> getMatchingTickets(String orderId, String ticketId) async {
+    final ticketCollection = await getTicketsCollection();
+
+    final checkByOrder = isCheckedByOrder(orderId);
+    final matchigTickets = ticketCollection
+        .where(checkByOrder
+            ? (t) => t['orderId'] == orderId.toUpperCase()
+            : (t) => t['ticketId'] == ticketId.toLowerCase())
+        .toList();
+    return matchigTickets;
+  }
+
+  bool isCheckedByOrder(String orderId) {
+    return orderId.length > 1;
+  }
+
+  Future<List> getTicketsCollection() async {
+    final tickets =
+        await Firestore.instance.document('tickets/tickets').snapshots().first;
+
+    final List ticketCollection = tickets.data['tickets'];
+    return ticketCollection;
   }
 }
